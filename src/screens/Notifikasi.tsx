148957b99f2de9288e7f4b63c731d316ddee625f
mhmdnurf <mhmd.nurf@gmail.com>
2024-04-19 01:39:55 +0700

    add fetching data on notifikasi

--- src/screens/Notifikasi.tsx
+++ src/screens/Notifikasi.tsx
@@ -1,31 +1,65 @@
 import React from 'react';
 import Header from '../components/Header';
-import {FlatList, StyleSheet, Text, View} from 'react-native';
+import {FlatList, RefreshControl, StyleSheet, Text, View} from 'react-native';
 import FlatContainer from '../components/FlatContainer';
 import NotifikasiCard from '../components/notifikasi/NotifikasiCard';
 import BottomSpace from '../components/BottomSpace';
-
+import firestore from '@react-native-firebase/firestore';
+import auth from '@react-native-firebase/auth';
+import {useIsFocused} from '@react-navigation/native';
 const Notifikasi = () => {
-  const data = [
-    {
-      user_uid: '1',
-      title: 'Pembayaran Berhasil',
-      pesan: 'Pembayaran untuk lapangan 1 berhasil',
-      status: 'success',
-    },
-    {
-      user_uid: '1',
-      title: 'Pembayaran Gagal',
-      pesan: 'Pembayaran untuk lapangan 1 berhasil',
-      status: 'failed',
-    },
-  ];
+  const isFocused = useIsFocused();
+  const [data, setData] = React.useState([]);
+  const [refreshing, setRefreshing] = React.useState(false);
+  // const data = [
+  //   {
+  //     user_uid: '1',
+  //     title: 'Pembayaran Berhasil',
+  //     pesan: 'Pembayaran untuk lapangan 1 berhasil',
+  //     status: 'success',
+  //   },
+  //   {
+  //     user_uid: '1',
+  //     title: 'Pembayaran Gagal',
+  //     pesan: 'Pembayaran untuk lapangan 1 berhasil',
+  //     status: 'failed',
+  //   },
+  // ];
+
+  const fetchNotifikasi = React.useCallback(async () => {
+    setRefreshing(true);
+    try {
+      const user = auth().currentUser;
+      const notifikasi = await firestore()
+        .collection('notifikasi')
+        .where('user_uid', '==', user.uid)
+        .get();
+      const dataNotifikasi = notifikasi.docs.map(doc => doc.data());
+      setData(dataNotifikasi);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setRefreshing(false);
+    }
+  }, []);
+
+  React.useEffect(() => {
+    if (isFocused) {
+      fetchNotifikasi();
+    }
+  }, [fetchNotifikasi, isFocused]);
   return (
     <FlatContainer backgroundColor="white">
       <Header title="Notifikasi" />
       <View style={styles.container}>
         <Text style={styles.title}>Informasi Terkini</Text>
         <FlatList
+          refreshControl={
+            <RefreshControl
+              refreshing={refreshing}
+              onRefresh={fetchNotifikasi}
+            />
+          }
           data={data}
           renderItem={({item, index}) => (
             <NotifikasiCard
